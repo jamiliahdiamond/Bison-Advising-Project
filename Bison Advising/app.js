379b5c13890562ead68d5d2237d285d2a773025f
Jamiliah Eubanks <131091248+jamiliahdiamond@users.noreply.github.com>
2023-12-11 15:52:12 -0500

    Add files via upload
    First upload. Login goes nowhere.

---
+++ Bison Advising/app.js
@@ -0,0 +1,22 @@
+//navigation bar animation
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+
+//login sign in animation 
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
